51af3ffab9ee62129c11f7cae5f50641734dfad6
AmmarIbrahem <ammarslm1994@gmail.com>
2020-06-23 17:08:00 +0200

    Publish-FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+
+import axios from 'axios'
+
+const baseURL = "http://localhost:61952/getData";
+const saveDataSheetURL = "http://localhost:61952/upload";
+
+async function saveUrl(id, url){
+  var bodyFormData = new FormData();
+  bodyFormData.set("sheetURL", url);
+  const result = await axios({
+    method:"POST",
+    url: `${saveDataSheetURL}/${id}`,
+    data:bodyFormData
+  }); 
+  console.log(result); 
+}
+
+function renderList(dataSheets){
+    if(dataSheets)
+    return dataSheets.map((dataSheet) => {
+        return (         
+          <tr className="" key={dataSheet.dataSheetId}>
+            <td>{dataSheet.productId}</td>
+            <td>{dataSheet.supplierId}</td>
+            <td>{dataSheet.dataSheetUrl}</td>
+            <td>{dataSheet.updatedAt}</td>
+            <td>{dataSheet.isValid? "Valid":""}</td>
+            <td><button onClick={()=>saveUrl(dataSheet.dataSheetId, dataSheet.dataSheetUrl)} >Save</button></td>
+          </tr>
+        );
+      });
+    return <div>No dataSheets Found</div>;
+}
+
 
 function App() {
+  let [dataSheets, setdataSheets] = useState([]);
+  useEffect(()=>{
+    async function getdataSheets(){
+      const result = await axios.get(baseURL);  
+      setdataSheets(result.data);
+    }
+    getdataSheets();
+  },[]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Product</th>
+      <th scope="col">Supplier</th>
+      <th scope="col">DataSheet</th>
+      <th scope="col">Updated At</th>
+      <th scope="col">Valid</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    {renderList(dataSheets)}
+  </tbody>
+</table>
     </div>
   );
 }
